d452244cc3cf4f20497b69edcca20dbdaac4aef2
Eduarda Ferreira Lima <137132951+flduda@users.noreply.github.com>
2025-05-01 10:22:13 -0700

    Add 02.py file to rep

---
+++ Downloads/Ex_Files_Programming_Foundations_Fundamentals/02.py
@@ -0,0 +1,13 @@
+print ("Hi!")
+
+name = input("What is your name?")
+print ("It's nice to meet you, ", name)
+
+answer = input("Are you enjoying the course?")
+
+if answer == "Yes":
+    print("That's good to hear!")
+
+else:
+    print ("Oh no! That makes me sad!")
+    
