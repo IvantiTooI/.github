1998f6c7786498a9d0bd080b38c19252a70cffec
Eduarda Ferreira Lima <137132951+flduda@users.noreply.github.com>
2025-05-01 10:59:32 -0700

    Add 03.py challenge file to repository

---
+++ Downloads/Ex_Files_Programming_Foundations_Fundamentals/03.py
@@ -0,0 +1,11 @@
+##guessing game 
+print("Hi!")
+
+favfood = input ("What is your favorite food?")
+
+if favfood == "Banana": 
+    print ("Yep! So amazing!")
+else: 
+    print("Yuck! That's not it!")
+
+print ("Thanks for playing!")
